1feb8d92bd4e4dfe5d643fd0775297e97724aae0
cesarricart <cesarricart@gmail.com>
2025-11-13 16:26:50 -0300

    commit inicial

---
+++ src/main/java/br/com/senai/backend/sistema_mercado/models/Mercadoria.java
@@ -0,0 +1,68 @@
+package br.com.senai.backend.sistema_mercado.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "mercadoria")
+
+public class Mercadoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "quantidade")
+    private String quantidade;
+    @Column(name = "categoria")
+    private String categoria;
+
+    public Mercadoria() {
+    }
+
+    public Mercadoria(String categoria, Integer id, String nome, String quantidade) {
+        this.categoria = categoria;
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+}
